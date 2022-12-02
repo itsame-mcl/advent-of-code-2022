ecf01c39eff4ef688bc05b4b5b3c66dc09cf9958
Maxence Lagalle <contact@maxence-lagalle.fr>
2022-12-02 07:51:18 +0100

    Amélio import Jour 1

--- tests/day01_calorie_counting/test_part1_2.py
+++ tests/day01_calorie_counting/test_part1_2.py
@@ -1,22 +1,22 @@
 import unittest
-import src.day01_calorie_counting.part1_2 as src
+from src.day01_calorie_counting.part1_2 import max_calories
 
 
 class TestDay1(unittest.TestCase):
     def test_part1_with_sample(self):
-        res = src.max_calories("day01_calorie_counting/sample_part1_2.txt")
+        res = max_calories("day01_calorie_counting/sample_part1_2.txt")
         self.assertEqual(24000, res)
 
     def test_part1_with_input(self):
-        res = src.max_calories("day01_calorie_counting/input_part1_2.txt")
+        res = max_calories("day01_calorie_counting/input_part1_2.txt")
         self.assertEqual(69693, res)
 
     def test_part2_with_sample(self):
-        res = src.max_calories("day01_calorie_counting/sample_part1_2.txt", 3)
+        res = max_calories("day01_calorie_counting/sample_part1_2.txt", 3)
         self.assertEqual(45000, res)
 
     def test_part2_with_input(self):
-        res = src.max_calories("day01_calorie_counting/input_part1_2.txt", 3)
+        res = max_calories("day01_calorie_counting/input_part1_2.txt", 3)
         self.assertEqual(200945, res)
 
 
