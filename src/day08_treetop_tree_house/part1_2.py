c9081089fb5e9a2facd28fc7a6d8c98e9b96add5
Maxence Lagalle <maxence.lagalle@insee.fr>
2022-12-08 10:10:21 +0100

    Jour 8 - Décomposition en fonctions simples

--- src/day08_treetop_tree_house/part1_2.py
+++ src/day08_treetop_tree_house/part1_2.py
@@ -1,45 +1,53 @@
 import numpy as np
 
 
-def tree_house_computations(path, mode):
+def generate_grid(path):
     with open(path) as file:
         input_data = list(map(lambda l: l.replace('\n', ''), file.readlines()))
-    grid = np.array(list(map(lambda l: [int(x) for x in l], input_data)))
-    outcome = 0
+    return np.array(list(map(lambda l: [int(x) for x in l], input_data)))
+
+
+def grid_walker(grid, fun):
+    outcome = []
     for x in range(0, grid.shape[0]):
         for y in range(0, grid.shape[1]):
             north = grid[:x, y]
-            south = grid[x+1:, y]
+            south = grid[x + 1:, y]
             west = grid[x, :y]
-            east = grid[x, y+1:]
-            match mode:
-                case "visible_trees":
-                    if x in [0, grid.shape[0]-1] or y in [0, grid.shape[1]-1] or \
-                            grid[x, y] > min(0 if north.size == 0 else max(north), 0 if south.size == 0 else max(south),
-                                             0 if west.size == 0 else max(west), 0 if east.size == 0 else max(east)):
-                        outcome += 1
-                case "scenic_score":
-                    north_score = 0
-                    for tree in np.flip(north):
-                        north_score += 1
-                        if tree >= grid[x, y]:
-                            break
-                    west_score = 0
-                    for tree in np.flip(west):
-                        west_score += 1
-                        if tree >= grid[x, y]:
-                            break
-                    east_score = 0
-                    for tree in east:
-                        east_score += 1
-                        if tree >= grid[x, y]:
-                            break
-                    south_score = 0
-                    for tree in south:
-                        south_score += 1
-                        if tree >= grid[x, y]:
-                            break
-                    scenic_score = north_score * west_score * east_score * south_score
-                    if scenic_score > outcome:
-                        outcome = scenic_score
+            east = grid[x, y + 1:]
+            outcome.append(fun(grid[x, y], north, south, west, east))
     return outcome
+
+
+def tree_is_visible(current, north, south, west, east):
+    on_edge = min(north.size, south.size, west.size, east.size) == 0
+    is_visible = current > min(0 if north.size == 0 else max(north), 0 if south.size == 0 else max(south),
+                               0 if west.size == 0 else max(west), 0 if east.size == 0 else max(east))
+    return int(on_edge or is_visible)
+
+
+def visible_trees_from_pos(current, neighbours):
+    visibles = 0
+    for neighbour in neighbours:
+        visibles += 1
+        if neighbour >= current:
+            break
+    return visibles
+
+
+def scenic_score(current, north, south, west, east):
+    partials_scores = [visible_trees_from_pos(current, direction) for direction
+                       in [np.flip(north), np.flip(west), south, east]]
+    return int(np.prod(partials_scores))
+
+
+def number_of_visible_trees(path):
+    grid = generate_grid(path)
+    outcome = grid_walker(grid, tree_is_visible)
+    return sum(outcome)
+
+
+def best_scenic_score(path):
+    grid = generate_grid(path)
+    outcome = grid_walker(grid, scenic_score)
+    return max(outcome)
--- tests/day08_treetop_tree_house/test_part1_2.py
+++ tests/day08_treetop_tree_house/test_part1_2.py
@@ -1,6 +1,6 @@
 import unittest
 import os
-from src.day08_treetop_tree_house.part1_2 import tree_house_computations
+from src.day08_treetop_tree_house.part1_2 import number_of_visible_trees, best_scenic_score
 
 
 class TestDay8(unittest.TestCase):
@@ -8,19 +8,19 @@ class TestDay8(unittest.TestCase):
         self.dirname = os.path.dirname(__file__)
 
     def test_part1_with_sample(self):
-        res = tree_house_computations(os.path.join(self.dirname, "sample_part1_2.txt"), "visible_trees")
+        res = number_of_visible_trees(os.path.join(self.dirname, "sample_part1_2.txt"))
         self.assertEqual(21, res)
 
     def test_part1_with_input(self):
-        res = tree_house_computations(os.path.join(self.dirname, "input_part1_2.txt"), "visible_trees")
+        res = number_of_visible_trees(os.path.join(self.dirname, "input_part1_2.txt"))
         self.assertEqual(1843, res)
 
     def test_part2_with_sample(self):
-        res = tree_house_computations(os.path.join(self.dirname, "sample_part1_2.txt"), "scenic_score")
+        res = best_scenic_score(os.path.join(self.dirname, "sample_part1_2.txt"))
         self.assertEqual(8, res)
 
     def test_part2_with_input(self):
-        res = tree_house_computations(os.path.join(self.dirname, "input_part1_2.txt"), "scenic_score")
+        res = best_scenic_score(os.path.join(self.dirname, "input_part1_2.txt"))
         self.assertEqual(180000, res)
 
 
