a873e7f852482329fbd1a77c6190c823a8f4ac2f
Maxence Lagalle <contact@maxence-lagalle.fr>
2022-12-11 21:01:55 +0100

    Jour 11 - Simplification de la Partie 2

--- src/day11_monkey_in_the_middle/monkey.py
+++ src/day11_monkey_in_the_middle/monkey.py
@@ -1,4 +1,4 @@
-from copy import deepcopy
+from numpy import prod
 
 
 class Monkey:
@@ -21,6 +21,7 @@ class Monkey:
     def inspect_item(self, item):
         new = self.operation(item)
         new = new//3 if self.group.worry_decrease else new
+        new = new % prod([monkey.divisible_by for monkey in self.group.monkeys])
         other = self.if_true if new % self.divisible_by == 0 else self.if_false
         self.throw_to_other(new, other)
         self.items.remove(item)
@@ -31,35 +32,13 @@ class Monkey:
             self.inspect_item(item)
 
 
-class ZNZMonkey(Monkey):
-    def inspect_item(self, item):
-        match self.operation.split(" "):
-            case["+", value]:
-                item.add(int(value))
-            case["*", value]:
-                item.multiply(int(value))
-            case["^", value]:
-                item.power(int(value))
-            case _:
-                raise ValueError
-        other = self.if_true if item.get_value(self.divisible_by) == 0 else self.if_false
-        self.throw_to_other(deepcopy(item), other)
-        self.items.remove(item)
-        self.inspected += 1
-
-
 class MonkeyController:
-    def __init__(self, worry_decrease=True, znz_monkeys=False):
+    def __init__(self, worry_decrease=True):
         self.monkeys = []
         self.worry_decrease = worry_decrease
-        self.zn_monkeys = znz_monkeys
 
     def add_monkey(self, items, operation, divisible_by, if_true, if_false):
-        if self.zn_monkeys:
-            new_monkey = ZNZMonkey(self, items, operation, divisible_by, if_true, if_false)
-
-        else:
-            new_monkey = Monkey(self, items, operation, divisible_by, if_true, if_false)
+        new_monkey = Monkey(self, items, operation, divisible_by, if_true, if_false)
         self.monkeys.append(new_monkey)
 
     def get_monkey(self, index):
--- tests/day11_monkey_in_the_middle/test_part2.py
+++ tests/day11_monkey_in_the_middle/test_part2.py
@@ -1,68 +1,29 @@
 import unittest
 from src.day11_monkey_in_the_middle.monkey import MonkeyController
-from src.day11_monkey_in_the_middle.znz_integer import ZNZInteger
 
 
 class TestDay11Part2(unittest.TestCase):
 
-    def test_part2_with_invalid_sample(self):
-        monkeys = MonkeyController(False, True)
-        monkeys.add_monkey([ZNZInteger(79, [13, 17, 19, 23]), ZNZInteger(98, [13, 17, 19, 23])],
-                           "/ 2", 23, 0, 0)
-        self.assertRaises(ValueError, monkeys.do_rounds)
-
     def test_part2_with_sample(self):
-        monkeys = MonkeyController(False, True)
-        monkeys.add_monkey([ZNZInteger(79, [13, 17, 19, 23]), ZNZInteger(98, [13, 17, 19, 23])],
-                           "* 19", 23, 2, 3)
-        monkeys.add_monkey([ZNZInteger(54, [13, 17, 19, 23]), ZNZInteger(65, [13, 17, 19, 23]),
-                            ZNZInteger(75, [13, 17, 19, 23]), ZNZInteger(74, [13, 17, 19, 23])],
-                           "+ 6", 19, 2, 0)
-        monkeys.add_monkey([ZNZInteger(79, [13, 17, 19, 23]), ZNZInteger(60, [13, 17, 19, 23]),
-                            ZNZInteger(97, [13, 17, 19, 23])], "^ 2", 13, 1, 3)
-        monkeys.add_monkey([ZNZInteger(74, [13, 17, 19, 23])], "+ 3", 17, 0, 1)
+        monkeys = MonkeyController(False)
+        monkeys.add_monkey([79, 98], lambda old: old * 19, 23, 2, 3)
+        monkeys.add_monkey([54, 65, 75, 74], lambda old: old + 6, 19, 2, 0)
+        monkeys.add_monkey([79, 60, 97], lambda old: old * old, 13, 1, 3)
+        monkeys.add_monkey([74], lambda old: old + 3, 17, 0, 1)
         monkeys.do_rounds(10000)
         score = monkeys.compute_monkey_business_level()
         self.assertEqual(2713310158, score)
 
     def test_part2_with_input(self):
-        monkeys = MonkeyController(False, True)
-        monkeys.add_monkey([ZNZInteger(91, [2, 3, 5, 7, 11, 13, 17, 19]),
-                            ZNZInteger(54, [2, 3, 5, 7, 11, 13, 17, 19]),
-                            ZNZInteger(70, [2, 3, 5, 7, 11, 13, 17, 19]),
-                            ZNZInteger(61, [2, 3, 5, 7, 11, 13, 17, 19]),
-                            ZNZInteger(64, [2, 3, 5, 7, 11, 13, 17, 19]),
-                            ZNZInteger(64, [2, 3, 5, 7, 11, 13, 17, 19]),
-                            ZNZInteger(60, [2, 3, 5, 7, 11, 13, 17, 19]),
-                            ZNZInteger(85, [2, 3, 5, 7, 11, 13, 17, 19])], "* 13", 2, 5, 2)
-        monkeys.add_monkey([ZNZInteger(82, [2, 3, 5, 7, 11, 13, 17, 19])], "+ 7", 13, 4, 3)
-        monkeys.add_monkey([ZNZInteger(84, [2, 3, 5, 7, 11, 13, 17, 19]),
-                            ZNZInteger(93, [2, 3, 5, 7, 11, 13, 17, 19]),
-                            ZNZInteger(70, [2, 3, 5, 7, 11, 13, 17, 19])], "+ 2", 5, 5, 1)
-        monkeys.add_monkey([ZNZInteger(78, [2, 3, 5, 7, 11, 13, 17, 19]),
-                            ZNZInteger(56, [2, 3, 5, 7, 11, 13, 17, 19]),
-                            ZNZInteger(85, [2, 3, 5, 7, 11, 13, 17, 19]),
-                            ZNZInteger(93, [2, 3, 5, 7, 11, 13, 17, 19])], "* 2", 3, 6, 7)
-        monkeys.add_monkey([ZNZInteger(64, [2, 3, 5, 7, 11, 13, 17, 19]),
-                            ZNZInteger(57, [2, 3, 5, 7, 11, 13, 17, 19]),
-                            ZNZInteger(81, [2, 3, 5, 7, 11, 13, 17, 19]),
-                            ZNZInteger(95, [2, 3, 5, 7, 11, 13, 17, 19]),
-                            ZNZInteger(52, [2, 3, 5, 7, 11, 13, 17, 19]),
-                            ZNZInteger(71, [2, 3, 5, 7, 11, 13, 17, 19]),
-                            ZNZInteger(58, [2, 3, 5, 7, 11, 13, 17, 19])], "^ 2", 11, 7, 3)
-        monkeys.add_monkey([ZNZInteger(58, [2, 3, 5, 7, 11, 13, 17, 19]),
-                            ZNZInteger(71, [2, 3, 5, 7, 11, 13, 17, 19]),
-                            ZNZInteger(96, [2, 3, 5, 7, 11, 13, 17, 19]),
-                            ZNZInteger(58, [2, 3, 5, 7, 11, 13, 17, 19]),
-                            ZNZInteger(68, [2, 3, 5, 7, 11, 13, 17, 19]),
-                            ZNZInteger(90, [2, 3, 5, 7, 11, 13, 17, 19])], "+ 6", 17, 4, 1)
-        monkeys.add_monkey([ZNZInteger(56, [2, 3, 5, 7, 11, 13, 17, 19]),
-                            ZNZInteger(99, [2, 3, 5, 7, 11, 13, 17, 19]),
-                            ZNZInteger(89, [2, 3, 5, 7, 11, 13, 17, 19]),
-                            ZNZInteger(97, [2, 3, 5, 7, 11, 13, 17, 19]),
-                            ZNZInteger(81, [2, 3, 5, 7, 11, 13, 17, 19])], "+ 1", 7, 0, 2)
-        monkeys.add_monkey([ZNZInteger(68, [2, 3, 5, 7, 11, 13, 17, 19]),
-                            ZNZInteger(72, [2, 3, 5, 7, 11, 13, 17, 19])], "+ 8", 19, 6, 0)
+        monkeys = MonkeyController(False)
+        monkeys.add_monkey([91, 54, 70, 61, 64, 64, 60, 85], lambda old: old * 13, 2, 5, 2)
+        monkeys.add_monkey([82], lambda old: old + 7, 13, 4, 3)
+        monkeys.add_monkey([84, 93, 70], lambda old: old + 2, 5, 5, 1)
+        monkeys.add_monkey([78, 56, 85, 93], lambda old: old * 2, 3, 6, 7)
+        monkeys.add_monkey([64, 57, 81, 95, 52, 71, 58], lambda old: old * old, 11, 7, 3)
+        monkeys.add_monkey([58, 71, 96, 58, 68, 90], lambda old: old + 6, 17, 4, 1)
+        monkeys.add_monkey([56, 99, 89, 97, 81], lambda old: old + 1, 7, 0, 2)
+        monkeys.add_monkey([68, 72], lambda old: old + 8, 19, 6, 0)
         monkeys.do_rounds(10000)
         score = monkeys.compute_monkey_business_level()
         self.assertEqual(16792940265, score)
