9b9bafb70d4462ff59d98d2fdcc5aeaf8016a6cd
Maxence Lagalle <contact@maxence-lagalle.fr>
2022-12-16 20:06:11 +0100

    Jour 15 - Optimisations et solution exacte pour la partie 1 dans tous les cas

--- src/day15_beacon_exclusion_zone/part1_2.py
+++ src/day15_beacon_exclusion_zone/part1_2.py
@@ -25,30 +25,24 @@ def construct_polygons(path):
 def compute_single_row_exclusion(path, row):
     polygons = unary_union(construct_polygons(path))
     with open(path) as file:
-        x_min = None
-        x_max = None
         sensors_on_y = set()
         for line in file:
             positions = findall(r'-?\d+', line)
-            sensor = int(positions[0])+int(positions[1])*1j
             beacon = int(positions[2])+int(positions[3])*1j
             if int(beacon.imag) == row:
-                sensors_on_y.add(beacon.real)
-            distance = manhattan_distance(sensor, beacon)
-            if not x_min or sensor.real - distance < x_min:
-                x_min = sensor.real - distance
-            if not x_max or sensor.real+distance > x_max:
-                x_max = sensor.real + distance
+                sensors_on_y.add(LineString([(beacon.real-0.5, row), (beacon.real+0.5, row)]))
+        x_min, _, x_max, _ = polygons.envelope.bounds
         target_row = LineString([(x_min, row), (x_max, row)])
-        inter = polygons.intersection(target_row)
-    return inter.length + 1 - len(sensors_on_y)
+        sensors_on_y = unary_union(list(sensors_on_y))
+        res = polygons.intersection(target_row)
+        res = res.difference(sensors_on_y)
+    return res.length + 1
 
 
 def find_distress_beacon(path, x_y_max):
     polygons = unary_union(construct_polygons(path))
     grid = Polygon([(0, 0), (0, x_y_max), (x_y_max, x_y_max), (x_y_max, 0)])
-    polygons_on_grid = grid.intersection(polygons)
-    distress_beacon = grid.difference(polygons_on_grid)
+    distress_beacon = grid.difference(polygons)
     distress_beacon = distress_beacon.geoms[0] if isinstance(distress_beacon, MultiPolygon) else distress_beacon
     x, y = distress_beacon.centroid.coords.xy
     return int(x[0]) * 4000000 + int(y[0])
