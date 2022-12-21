a8985166158bce41544b4f10a9a6cee2c3d9b4fa
Maxence Lagalle <contact@maxence-lagalle.fr>
2022-12-21 19:32:19 +0100

    Jour 21 - Résolution avec Z3

--- src/day21_monkey_math/part1_2.py
+++ src/day21_monkey_math/part1_2.py
@@ -1,81 +1,60 @@
-import math
 from collections import namedtuple
+from typing import Union
 from parse import compile
-from copy import copy
+from z3 import Real, Solver
 
-Monkey = namedtuple("Monkey", "first second operation")
+OperationMonkey = namedtuple("OperationMonkey", "first second operation")
+ValueMonkey = namedtuple("ValueMonkey", "value")
 
 
 def get_monkeys(path):
     parse_value = compile("{name:4l}: {value:d}")
-    parse_operation = compile("{name:4l}: {first:4l} {operation_type} {second:4l}")
-    operations: dict[str, Monkey] = {}
-    values: dict[str, int] = {}
+    parse_operation = compile("{name:4l}: {first:4l} {operation} {second:4l}")
+    monkeys: dict[str, Union[ValueMonkey, OperationMonkey]] = {}
     with open(path) as file:
         for line in file:
             line = line.replace('\n', '')
             with_value = parse_value.parse(line)
             with_operation = parse_operation.parse(line)
             if with_value:
-                values[with_value.named['name']] = with_value.named['value']
+                monkeys[with_value.named['name']] = ValueMonkey(value=with_value.named['value'])
             if with_operation:
-                match with_operation.named['operation_type']:
-                    case '+':
-                        op = lambda first, second: first + second
-                    case '-':
-                        op = lambda first, second: first - second
-                    case '*':
-                        op = lambda first, second: first * second
-                    case '/':
-                        op = lambda first, second: first / second
-                operations[with_operation.named['name']] = Monkey(first=with_operation.named['first'],
-                                                                  second=with_operation.named['second'],
-                                                                  operation=op)
-    return values, operations
+                monkeys[with_operation.named['name']] = OperationMonkey(first=with_operation.named['first'],
+                                                                        second=with_operation.named['second'],
+                                                                        operation=with_operation.named['operation'])
+    return monkeys
 
 
-def compute_root_value(values, operations, human=None):
-    local_values = copy(values)
-    local_operations = copy(operations)
-    if human:
-        local_values['humn'] = human
-        local_operations['root'] = Monkey._replace(local_operations['root'],
-                                                   operation=lambda first, second: first - second)
-    while 'root' not in local_values:
-        operations_round = copy(local_operations)
-        for name, operation in operations_round.items():
-            if operation.first in local_values and operation.second in local_values:
-                local_values[name] = operation.operation(local_values[operation.first], local_values[operation.second])
-                del local_operations[name]
-    return local_values['root']
+def set_problem_and_solve(monkeys: dict[str, Union[ValueMonkey, OperationMonkey]], objective):
+    z3_monkeys = {}
+    solver = Solver()
+    for monkey_name in monkeys.keys():
+        z3_monkeys[monkey_name] = Real(monkey_name)
+    if objective == 'humn':
+        monkeys['root'] = OperationMonkey._replace(monkeys['root'], operation='=')
+        del monkeys['humn']
+        del z3_monkeys['root']
+    for name, monkey in monkeys.items():
+        if isinstance(monkey, ValueMonkey):
+            solver.add(z3_monkeys[name] == monkey.value)
+        if isinstance(monkey, OperationMonkey):
+            match monkey.operation:
+                case '+':
+                    solver.add(z3_monkeys[name] == z3_monkeys[monkey.first] + z3_monkeys[monkey.second])
+                case '-':
+                    solver.add(z3_monkeys[name] == z3_monkeys[monkey.first] - z3_monkeys[monkey.second])
+                case '*':
+                    solver.add(z3_monkeys[name] == z3_monkeys[monkey.first] * z3_monkeys[monkey.second])
+                case '/':
+                    solver.add(z3_monkeys[name] == z3_monkeys[monkey.first] / z3_monkeys[monkey.second])
+                case '=':
+                    solver.add(z3_monkeys[monkey.first] == z3_monkeys[monkey.second])
+                case _:
+                    raise ValueError
+    solver.check()
+    return solver.model()[z3_monkeys[objective]].as_long()
 
 
-def run_computation(path):
-    values, operations = get_monkeys(path)
-    return compute_root_value(values, operations)
-
-
-def solve_root_equality(path):
-    values, operations = get_monkeys(path)
-    space_dimension = 1
-    lower_bound = -1
-    higher_bound = 1
-    # Assume the function is monotonous
-    is_increasing = compute_root_value(values, operations, higher_bound) >\
-                    compute_root_value(values, operations, lower_bound)
-    while compute_root_value(values, operations, lower_bound) *\
-            compute_root_value(values, operations, higher_bound) > 0:
-        space_dimension += 1
-        lower_bound = -math.pow(10, space_dimension)
-        higher_bound = math.pow(10, space_dimension)
-    root_difference, dichotomous_value = None, None
-    while root_difference != 0:
-        dichotomous_value = (lower_bound + higher_bound) // 2
-        # Assume 0 is a discontinuity of the function
-        dichotomous_value = 1 if dichotomous_value == 0 else dichotomous_value
-        root_difference = compute_root_value(values, operations, dichotomous_value)
-        if (is_increasing and root_difference < 0) or (not is_increasing and root_difference > 0):
-            lower_bound = dichotomous_value + 1
-        else:
-            higher_bound = dichotomous_value - 1
-    return dichotomous_value
+def solve_with_z3(path, objective):
+    monkeys = get_monkeys(path)
+    return set_problem_and_solve(monkeys, objective)
--- tests/day21_monkey_math/test_part1_2.py
+++ tests/day21_monkey_math/test_part1_2.py
@@ -1,26 +1,29 @@
 import unittest
 import os
-from src.day21_monkey_math.part1_2 import run_computation, solve_root_equality
+from src.day21_monkey_math.part1_2 import solve_with_z3
 
 
 class TestDay21(unittest.TestCase):
     def setUp(self):
         self.dirname = os.path.dirname(__file__)
 
+    def test_with_invalid_sample(self):
+        self.assertRaises(ValueError, solve_with_z3, os.path.join(self.dirname, "invalid_sample_part1_2.txt"), 'root')
+
     def test_part1_with_sample(self):
-        res = run_computation(os.path.join(self.dirname, "sample_part1_2.txt"))
+        res = solve_with_z3(os.path.join(self.dirname, "sample_part1_2.txt"), 'root')
         self.assertEqual(152, res)
 
     def test_part1_with_input(self):
-        res = run_computation(os.path.join(self.dirname, "input_part1_2.txt"))
+        res = solve_with_z3(os.path.join(self.dirname, "input_part1_2.txt"), 'root')
         self.assertEqual(331319379445180, res)
 
     def test_part2_with_sample(self):
-        res = solve_root_equality(os.path.join(self.dirname, "sample_part1_2.txt"))
+        res = solve_with_z3(os.path.join(self.dirname, "sample_part1_2.txt"), 'humn')
         self.assertEqual(301, res)
 
     def test_part2_with_input(self):
-        res = solve_root_equality(os.path.join(self.dirname, "input_part1_2.txt"))
+        res = solve_with_z3(os.path.join(self.dirname, "input_part1_2.txt"), 'humn')
         self.assertEqual(3715799488132, res)
 
 
