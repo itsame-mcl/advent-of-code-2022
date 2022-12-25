64578569a9a3a7ec6bd1a2dbabfde1d0bacb1de8
Maxence Lagalle <contact@maxence-lagalle.fr>
2022-12-25 09:39:17 +0100

    Jour 25

---
+++ src/day25_full_of_hot_air/snafu_decimal.py
@@ -0,0 +1,47 @@
+from numpy import base_repr
+from math import pow
+
+snafu_equivalences = {'2': 2, '1': 1, '0': 0, '-': -1, '=': -2}
+
+
+def snafu_to_decimal(snafu):
+    decimal = 0
+    for i in range(1, len(snafu)+1):
+        decimal += snafu_equivalences[snafu[-i]] * pow(5, i-1)
+    return decimal
+
+
+def decimal_to_snafu(number):
+    on_base_5 = list(base_repr(number, 5))
+    on_snafu = ''
+    for i in range(1, len(on_base_5) + 1):
+        match on_base_5[-i]:
+            case '0' | '1' | '2':
+                on_snafu = on_base_5[-i] + on_snafu
+            case '3':
+                if i < len(on_base_5):
+                    on_snafu = '=' + on_snafu
+                    on_base_5[-i-1] = str(int(on_base_5[-i-1]) + 1)
+                else:
+                    on_snafu = '1=' + on_snafu
+            case '4':
+                if i < len(on_base_5):
+                    on_snafu = '-' + on_snafu
+                    on_base_5[-i - 1] = str(int(on_base_5[-i - 1]) + 1)
+                else:
+                    on_snafu = '1-' + on_snafu
+            case '5':
+                if i < len(on_base_5):
+                    on_snafu = '0' + on_snafu
+                    on_base_5[-i - 1] = str(int(on_base_5[-i - 1]) + 1)
+                else:
+                    on_snafu = '10' + on_snafu
+    return on_snafu
+
+
+def sum_snafu(path):
+    with open(path) as file:
+        snafu_numbers = list(map(lambda l: l.replace('\n', ''), file.readlines()))
+    total_converted_snafu = int(sum(list(map(lambda snafu: snafu_to_decimal(snafu), snafu_numbers))))
+    total_snafu = decimal_to_snafu(total_converted_snafu)
+    return total_snafu
---
+++ tests/day25_full_of_hot_air/test_snafu_decimal.py
@@ -0,0 +1,37 @@
+import unittest
+import os
+from src.day25_full_of_hot_air.snafu_decimal import sum_snafu, decimal_to_snafu
+
+
+class TestDay25(unittest.TestCase):
+    def setUp(self):
+        self.dirname = os.path.dirname(__file__)
+
+    def test_decimal_to_snafu(self):
+        self.assertEqual('1', decimal_to_snafu(1))
+        self.assertEqual('2', decimal_to_snafu(2))
+        self.assertEqual('1=', decimal_to_snafu(3))
+        self.assertEqual('1-', decimal_to_snafu(4))
+        self.assertEqual('10', decimal_to_snafu(5))
+        self.assertEqual('11', decimal_to_snafu(6))
+        self.assertEqual('12', decimal_to_snafu(7))
+        self.assertEqual('2=', decimal_to_snafu(8))
+        self.assertEqual('2-', decimal_to_snafu(9))
+        self.assertEqual('20', decimal_to_snafu(10))
+        self.assertEqual('1=0', decimal_to_snafu(15))
+        self.assertEqual('1-0', decimal_to_snafu(20))
+        self.assertEqual('1=11-2', decimal_to_snafu(2022))
+        self.assertEqual('1-0---0', decimal_to_snafu(12345))
+        self.assertEqual('1121-1110-1=0', decimal_to_snafu(314159265))
+
+    def test_with_sample(self):
+        res = sum_snafu(os.path.join(self.dirname, "sample.txt"))
+        self.assertEqual('2=-1=0', res)
+
+    def test_with_input(self):
+        res = sum_snafu(os.path.join(self.dirname, "input.txt"))
+        self.assertEqual('2-2=21=0021=-02-1=-0', res)
+
+
+if __name__ == '__main__':
+    unittest.main()
